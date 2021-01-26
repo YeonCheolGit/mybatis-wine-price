2a33a8ac2939d4713d32a19a78204261567df376
YeonCheolJang <yeoncheol.jang@gmail.com>
2021-01-26 21:20:38 +0900

    minor update

--- src/main/controller/WineController.java
+++ src/main/controller/WineController.java
@@ -64,29 +64,4 @@ public class WineController {
         logger.debug("search >>> ");
         return wineService.search(request.getParameter("term"));
     }
-
-//    @GetMapping(value = "/crawler")
-//    public void crawler(HttpServletRequest request) throws IOException {
-//        logger.debug("crawler >>> ");
-//
-//        int number = 0;
-//
-//        while (number <= 7) {
-//            ++number;
-//            Document doc1 = Jsoup
-//                    .connect("http://www.ssg.com/search.ssg?target=all&query=와인&ctgId=6000099422&ctgLv=3&ctgLast=Y&parentCtgId=6000099420&count=100&page=" + number).get();
-//            Elements title1 = doc1.select("a.clickable > em.tx_ko");
-//
-//            int count = 0;
-//
-//            for (Element element : title1) {
-//                count++;
-//                String win = element.text();
-//                wineService.addWines(win);
-//                System.out.println(count + " >> " + win + " >> " + count);
-//            }
-//            System.out.println(number + "페이지 끝");
-//            System.out.println("======================= \n");
-//        }
-//    }
 }
